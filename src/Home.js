dbd6ac2bec71d4d488a62140182a460fdd605893
MadeneMeriem <meriem.mdn2005@gmail.com>
2025-08-17 06:34:34 +0100

    Error handling

--- src/Home.js
+++ src/Home.js
@@ -2,15 +2,10 @@
 import { useState,useEffect } from 'react';
 import BlogList from './BlogList';
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome to the bread bank' , body: 'Here we sell bread, we sell loaf', author: 'luigi', id: 2 },
-        { title: 'Web dev tips', body: 'lorem ipsum...', author: 'yoshi', id: 3 },
-        { title: 'React vs Vue', body: 'lorem ipsum...', author: 'mario', id: 4 },
-        { title: 'CSS Tricks', body: 'lorem ipsum...', author: 'luigi', id: 5 },
-        { title: 'JavaScript Basics', body: 'lorem ipsum...', author: 'yoshi', id: 6 },
-        { title: 'Understanding Hooks', body: 'lorem ipsum...', author: 'mario', id: 7 },
-    ])
+    const [blogs, setBlogs] = useState([]);
+    const [isLoading, setisLoadging] = useState(true);
+    const [error, setError] = useState(null);
+ 
 
     const handleDelete= (id) =>{
         const newBlogs = blogs.filter(blog => blog.id !==id);
@@ -18,12 +13,31 @@ const Home = () => {
     }
 
     useEffect(() => {
-        console.log('useEffect ran'); 
-    })
+        fetch('http://localhost:8100/blogs')
+        .then (response =>{
+            if(!response.ok){
+                throw Error('Could not fetch the data for that resource because of server side error');
+            }
+            return response.json();
+        })
+        .then (
+            data => {
+                setBlogs(data);
+                setisLoadging(false);
+                setError(null);
+            }
+        )
+        .catch(error => {
+            setError(error.message);
+            setisLoadging(false);
+        })
+    },[])
 
 
     return ( 
         <div className="home">
+            {error && <div>{error}</div>}
+            {isLoading && <div>Loading...</div>}
             <BlogList blogs={blogs} title = {"All blogs"} handleDelete={handleDelete}/>
             <BlogList blogs={blogs.filter((blog)=> blog.author === 'mario')} title={"Mario's blogs"} handleDelete={handleDelete}/>
         </div>
