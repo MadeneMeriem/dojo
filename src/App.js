a2a1c90f7297115365ad2059cf83e94dea7043be
MadeneMeriem <meriem.mdn2005@gmail.com>
2025-08-17 07:46:12 +0100

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import Navbar from './Navbar';
 import Home from './Home';
-
+import { BrowserRouter as Router , Route, Switch } from 'react-router-dom';
 function App() {
   const link = "https://reactjs.org";
   return (
-    <div className="app">
-       <Navbar />
-     <div className="content">
-      <Home />
-    <a href={link}>React website</a>
-    </div>
-    </div>
+      <Router>
+          <div className="app">
+        <Navbar />
+          <div className="content">
+          <Switch>
+            <Route exact  path='/'>
+              <Home />
+            </Route>
+            
+          </Switch>
+          </div>
+      </div>
+      </Router>
    
   );
 }
